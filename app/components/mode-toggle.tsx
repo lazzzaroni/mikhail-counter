ef33c11358da1654e22a6a6c9ce9efdacec2c184
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-12-28 22:14:58 +0100

    update mode-toggle component

--- app/components/mode-toggle.tsx
+++ app/components/mode-toggle.tsx
@@ -1,5 +1,5 @@
-import { Moon, Sun } from "lucide-react";
 import { Theme, useTheme } from "remix-themes";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
 
 import { Button } from "./ui/button";
 
@@ -8,14 +8,15 @@ export function ModeToggle() {
 
   return (
     <Button
-      variant="ghost"
+      variant="link"
       size="icon"
       onClick={() => {
         theme === "light" ? setTheme(Theme.DARK) : setTheme(Theme.LIGHT);
       }}
+      className="text-foreground no-underline hover:text-muted-foreground"
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <SunIcon className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
