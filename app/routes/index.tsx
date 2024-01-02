435a034affd97706461bc72a7911c3942ba17845
lazzzaroni <iamnotlazzaroni@gmail.com>
2024-01-02 09:17:37 +0100

    update counter value to 13 🎉

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
     <section className="mx-auto flex flex-col items-center justify-center gap-12">
-      <p className="font-mono text-9xl">12</p>
+      <p className="font-mono text-9xl">13</p>
     </section>
   );
 }
