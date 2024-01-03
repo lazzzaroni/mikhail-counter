aaf3308e27525185774487db31b183bc39a81a31
lazzzaroni <iamnotlazzaroni@gmail.com>
2024-01-03 22:03:35 +0100

    testing stage environment

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,7 +19,7 @@ export default function Index() {
 
   return (
     <section className="mx-auto flex flex-col items-center justify-center gap-12">
-      <p className="font-mono text-9xl">{count}</p>
+      <p className="font-mono text-8xl tracking-tighter">{`{${count}}`}</p>
       <p className="font-mono text-lg">
         {`updated `}
         <span className="text-muted-foreground">
